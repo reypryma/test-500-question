dcb48387e54b7ffa142770383e015d97a8a3550c
reypryma <refanopradana@gmail.com>
2024-05-21 13:24:29 +0700

    solve find number factor

--- src/main/java/org/example/easy/A050NumberTheory/ExerciseA050.java
+++ src/main/java/org/example/easy/A050NumberTheory/ExerciseA050.java
@@ -6,20 +6,34 @@ import java.util.List;
 
 public class ExerciseA050 {
     public static List<Integer> findFactors(int num) {
+        if (num < 0) {
+            return new ArrayList<>();
+        }
+
         List<Integer> factors = new ArrayList<>();
+
         for (int i = 1; i <= Math.sqrt(Math.abs(num)); i++) {
             if (num % i == 0) {
                 factors.add(i);
+                // check if i is not multiplied number of the num
                 if (i != num / i) {
                     factors.add(Math.abs(num) / i);
                 }
             }
         }
+
         Collections.sort(factors);
         return factors;
     }
 
     public static void main(String[] args) {
-        // Test your function with sample inputs
+        // expected [1, 2, 4, 7, 14, 28]
+        System.out.println(findFactors(28));
+        // expected [1, 2, 3, 6]
+        System.out.println(findFactors(6));
+        // expected [1, 5]
+        System.out.println(findFactors(5));
+        // expected [1, 2, 4, 8, 16]
+        System.out.println(findFactors(16));
     }
 }
