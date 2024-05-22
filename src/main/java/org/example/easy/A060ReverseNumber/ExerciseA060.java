3eaebf701670bccb5c8b07d56b7573cb655357ed
reypryma <refanopradana@gmail.com>
2024-05-22 09:58:27 +0700

    add reverse

--- src/main/java/org/example/easy/A060ReverseNumber/ExerciseA060.java
+++ src/main/java/org/example/easy/A060ReverseNumber/ExerciseA060.java
@@ -2,13 +2,11 @@ package org.example.easy.A060ReverseNumber;
 
 public class ExerciseA060 {
     public static int reverseNumber(int num) {
-        if(num < 0){
-            throw new IllegalArgumentException("no negative");
-        }
 
         int reverse = 0;
 
-        while(num > 0){
+        // !=0 allow num to be negative
+        while(num != 0){
             reverse = reverse * 10 + (num % 10);
             num /= 10;
         }
