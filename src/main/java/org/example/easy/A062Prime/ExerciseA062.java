471d3123a3b6a24403b6ba55a9f5469ecdb7f910
reypryma <refanopradana@gmail.com>
2024-05-22 10:34:43 +0700

    add prime

---
+++ src/main/java/org/example/easy/A062Prime/ExerciseA062.java
@@ -0,0 +1,30 @@
+package org.example.easy.A062Prime;
+
+public class ExerciseA062 {
+    public static boolean isPrime(int n) {
+        n = Math.abs(n);
+
+        if(n<4 && n>1){
+            return true;
+        } else if (n<=1 || n==4) {
+            return false;
+        } else if(n % 2 == 0){
+            return false;
+        }
+
+        for (int i = 5; i < n; i+=2) {
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // Test your function with sample inputs
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(4));
+        System.out.println(isPrime(17));
+        System.out.println(isPrime(25));
+    }
+}
