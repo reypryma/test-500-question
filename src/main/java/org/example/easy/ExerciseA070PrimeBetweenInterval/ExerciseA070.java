9a0a24d02b12ebedd00148968126473f310bc21a
reypryma <refanopradana@gmail.com>
2024-06-11 11:04:24 +0700

    update

---
+++ src/main/java/org/example/easy/ExerciseA070PrimeBetweenInterval/ExerciseA070.java
@@ -0,0 +1,35 @@
+package org.example.easy.ExerciseA070PrimeBetweenInterval;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExerciseA070 {
+    public static boolean isPrime(int num) {
+        // Your code to check primality
+        if(num <= 1) {
+            return false;
+        }
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<Integer> findPrimesBetweenIntervals(int start, int end) {
+        // Your code to find primes between intervals
+        List<Integer> primeNumber = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                primeNumber.add(i);
+            }
+        }
+        return primeNumber;
+    }
+
+    public static void main(String[] args) {
+        // Test your function with sample inputs
+        System.out.println(findPrimesBetweenIntervals(0, 1));
+    }
+}
