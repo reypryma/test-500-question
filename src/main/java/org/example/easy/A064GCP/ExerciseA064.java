64bc00af8744633c6451123688be86df84e5ca6a
reypryma <refanopradana@gmail.com>
2024-06-11 10:12:02 +0700

    gcd add other alternative

--- src/main/java/org/example/easy/A064GCP/ExerciseA064.java
+++ src/main/java/org/example/easy/A064GCP/ExerciseA064.java
@@ -33,6 +33,8 @@ public class ExerciseA064 {
         // Iter 3 gcd(14, 0)
 
         System.out.println(gcd(15, 5));
+        System.out.println(gcdLinear(15, 5));
         System.out.println(gcd(17, 19));
+        System.out.println(gcdLinear(17, 19));
     }
 }
