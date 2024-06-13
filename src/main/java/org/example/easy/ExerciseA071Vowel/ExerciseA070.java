2503ed5c0d1808a1a4f802de0e248861fa1aaa55
reypryma <refanopradana@gmail.com>
2024-06-13 09:12:09 +0700

    add vowel check

---
+++ src/main/java/org/example/easy/ExerciseA071Vowel/ExerciseA070.java
@@ -0,0 +1,46 @@
+package org.example.easy.ExerciseA071Vowel;
+
+public class ExerciseA070 {
+    public static int countVowels(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("String is null");
+        }
+
+        int count = 0;
+        String vowels = "AEIOUaeiou";
+
+        for (char c : s.toCharArray()) {
+            //indexof check if in array have the value exist at certain index
+            if (vowels.indexOf(c) != -1) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int countVowels2(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("String is null");
+        }
+
+        int count = 0;
+
+        String[] vowels = {"a", "i", "u", "e", "o"};
+
+        for (String charS : s.split("")) {
+            for (String v: vowels) {
+                if(charS.toLowerCase().equals(v)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String sampleString = "Hello, World!";
+        System.out.println(countVowels(sampleString));
+        System.out.println(countVowels2(sampleString));
+    }
+}
