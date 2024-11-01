1b6e6eaff7805412f8c287c971f91904f81e1d8e
reypryma <refanopradana@gmail.com>
2024-11-01 16:27:28 +0700

    expla

--- src/main/java/org/example/easy/A089BinarySearchOps/A089ExerciseJ.java
+++ src/main/java/org/example/easy/A089BinarySearchOps/A089ExerciseJ.java
@@ -15,9 +15,25 @@ public class A089ExerciseJ {
         int val, closest = root.val;
 
         while (root != null) {
+            // set value current to current root val
             val = root.val;
+            // check if range current closer than target
             closest = (Math.abs(val - target) < Math.abs(closest - target)) ? val : closest;
-            root = (root.val < target) ? root.right : root.left;
+            root = (root.val < target) ? root.right : root.left; // this soon will lead to null so the value of closest will be save at var closest
+        }
+
+        return closest;
+    }
+
+    public int closestValue2(TreeNode root, double target) {
+        int val, closest = root.val;
+
+        while (root != null) {
+            // set value current to current root val
+            val = root.val;
+            // check if current val closer than closest in target
+            closest = target < val ? closest : val;
+            root = (root.val < target) ? root.right : root.left; // this soon will lead to null
         }
 
         return closest;
@@ -33,5 +49,9 @@ public class A089ExerciseJ {
         root.left.right = ex.new TreeNode(3);
 
         System.out.println(ex.closestValue(root, 3.714286));
+        System.out.println(ex.closestValue2(root, 3.714286));
+
+        System.out.println(ex.closestValue(root, 6));
+        System.out.println(ex.closestValue2(root, 6));
     }
 }
