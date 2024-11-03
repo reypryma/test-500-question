aa256b7c17373638dd761122fe0325e850f4450e
reypryma <refanopradana@gmail.com>
2024-11-03 18:48:54 +0700

    solve range arrays

--- src/main/java/org/example/easy/A086StrobogrammaticNumber/A086.java
+++ src/main/java/org/example/easy/A086StrobogrammaticNumber/A086.java
@@ -3,13 +3,23 @@ package org.example.easy.A086StrobogrammaticNumber;
 public class A086 {
     public static boolean isStrobogrammatic(String num) {
         // Your code here
-        char[] chars = num.toCharArray();
+        char[] chars = num.replaceAll(" ", "").toCharArray();
 
+        int left = 0;
+        int right = chars.length - 1;
 
+        while (left < right) {
+            if (checkLeftAndRight(chars[left], chars[right] )) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 
-    private boolean checkLeftAndRight(String num) {
-
+    private static boolean checkLeftAndRight(char left, char right) {
+        return left == '6' && right == '9' || left == '9' && right == '6' || left == right;
     }
 
     public static void main(String[] args) {
