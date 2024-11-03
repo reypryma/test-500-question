eb7e30910f585b2b0ce79338e8b6bff04de01d51
reypryma <refanopradana@gmail.com>
2024-11-03 17:22:49 +0700

    solve range arrays

---
+++ src/main/java/org/example/easy/A086StrobogrammaticNumber/A086.java
@@ -0,0 +1,18 @@
+package org.example.easy.A086StrobogrammaticNumber;
+
+public class A086 {
+    public static boolean isStrobogrammatic(String num) {
+        // Your code here
+        char[] chars = num.toCharArray();
+
+
+    }
+
+    private boolean checkLeftAndRight(String num) {
+
+    }
+
+    public static void main(String[] args) {
+        // Test your function with sample inputs
+    }
+}
